85691b975f8a01ef867c22347782ae4be7174ecf
me <sitests@outlook.com>
2019-03-26 21:16:06 +1300

    New project

---
+++ LoadPage/Pages/Test/Index.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LoadPage.Pages.Test
+{
+    public class IndexModel : PageModel
+    {
+        public async Task<IActionResult> OnPostAsync()
+        {
+
+            return Page();
+
+            
+        }
+    }
+}
